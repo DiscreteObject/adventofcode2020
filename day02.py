6474c5afcf91683c7299fafa11770dd61b5c51c4
Taylor Hudson <taylor.a.hudson@gmail.com>
2020-12-03 21:18:24 -0800

    Refactor

--- day02.py
+++ day02.py
@@ -1,3 +1,19 @@
+"""
+Part 1
+To try to debug the problem, they have created a list (your puzzle input) of passwords (according to the corrupted database) and the corporate policy when that password was set.
+
+For example, suppose you have the following list:
+
+1-3 a: abcde
+1-3 b: cdefg
+2-9 c: ccccccccc
+
+Each line gives the password policy and then the password. The password policy indicates the lowest and highest number of times a given letter must appear for the password to be valid. For example, 1-3 a means that the password must contain a at least 1 time and at most 3 times.
+
+In the above example, 2 passwords are valid. The middle password, cdefg, is not; it contains no instances of b, but needs at least 1. The first and third passwords are valid: they contain one a or nine c, both within the limits of their respective policies.
+
+"""
+
 import re
 from collections import namedtuple
 
@@ -14,29 +30,38 @@ def parse_line(line):
         return None
 
 def validate_password_check(check):
-    found_matching = 0
-    for char in check.password:
-        if char == check.check_char:
-            found_matching += 1
-    
-    return found_matching >= check.min and found_matching <= check.max
+    if not check:
+        return False
 
-def find_valid_passwords(lines):
-    num_passed = 0
-    for line in lines:
-        check = parse_line(line)
-        if check is None:
-            continue
-        if validate_password_check(check):
-            num_passed += 1
+    matching_chars = sum([char == check.check_char for char in check.password])
 
-    return num_passed
+    return matching_chars >= check.min and matching_chars <= check.max
+
+def find_valid_passwords_1(lines):
+    return sum([int(validate_password_check(parse_line(line))) for line in lines])
+
+"""
+Part 2
+
+Each policy actually describes two positions in the password, where 1 means the first character, 2 means the second character, and so on. (Be careful; Toboggan Corporate Policies have no concept of "index zero"!) Exactly one of these positions must contain the given letter. Other occurrences of the letter are irrelevant for the purposes of policy enforcement.
+
+Given the same example list from above:
+
+    1-3 a: abcde is valid: position 1 contains a and position 3 does not.
+    1-3 b: cdefg is invalid: neither position 1 nor position 3 contains b.
+    2-9 c: ccccccccc is invalid: both position 2 and position 9 contain c.
+
+"""
+
+def find_valid_passwords_2(lines):
+    pass
 
 def main():
     with open('day02.txt') as f:
         lines = f.readlines()
 
-    print(find_valid_passwords(lines))
+    print(find_valid_passwords_1(lines))
+    print(find_valid_passwords_2(lines))
 
 if __name__ == '__main__':
     main()
